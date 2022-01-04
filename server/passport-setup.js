4f2a8dc1149e8868506834e09ca527c1e435525b
Eugene Lee <40117342+scc135@users.noreply.github.com>
2022-01-03 22:58:22 -0500

    Passportjs is working,  redirects to sql, will time out (#7)
    * added sql commands to run to create db
    
    * Updated eslint, webpack, and package.json to fix linting issues
    
    * added routers and controllers for sql, fixed webpack and package.json
    
    * working sql controllers
    
    * Update README.md
    
    * finished sql controllers and get calls to sql will return dummy data
    
    * Updated eslint, webpack, and package.json to fix linting issues (#3)
    
    * Updated eslint, webpack, and package.json to fix linting issues
    
    * added routers and controllers for sql, fixed webpack and package.json
    
    * working sql controllers
    
    * Update README.md
    
    * finished sql controllers and get calls to sql will return dummy data
    
    * fixed the passportjs stuff
    
    * attempt to oauth with sql (#5)
    
    * test tile
    
    * attempt at oauth with sql
    
    * Delete .env
    
    * Delete create.sql
    
    Co-authored-by: Quyen Calixto <quyencalixto@Quyens-MacBook-Pro.local>
    Co-authored-by: Eugene Lee <40117342+scc135@users.noreply.github.com>
    
    * merge to dev (#6)
    
    * added sql commands to run to create db
    
    * Updated eslint, webpack, and package.json to fix linting issues
    
    * added routers and controllers for sql, fixed webpack and package.json
    
    * working sql controllers
    
    * Update README.md
    
    * finished sql controllers and get calls to sql will return dummy data
    
    * Updated eslint, webpack, and package.json to fix linting issues (#3)
    
    * Updated eslint, webpack, and package.json to fix linting issues
    
    * added routers and controllers for sql, fixed webpack and package.json
    
    * working sql controllers
    
    * Update README.md
    
    * finished sql controllers and get calls to sql will return dummy data
    
    * fixed the passportjs stuff
    
    Co-authored-by: Quyen Calixto <89818245+QtieCoder@users.noreply.github.com>
    Co-authored-by: Quyen Calixto <quyencalixto@Quyens-MacBook-Pro.local>

--- server/passport-setup.js
+++ server/passport-setup.js
@@ -1,13 +1,21 @@
-import db from './models/sql.js'
+import query from './models/sql.js'
 import passport from 'passport';
 import GoogleStrategy from 'passport-google-oidc';
 
 import * as dotenv from 'dotenv'
 dotenv.config()
 
-const query = db.query;
 
 const { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } = process.env;
+
+passport.serializeUser((userID, done) => {
+    done(null, userID);
+});
+
+passport.deserializeUser((userID, done) => {
+    //find user then
+    done(null, userID);
+});
 
 // this sets up passport to use google
 const passportSetup = () => {
@@ -19,22 +27,68 @@ const passportSetup = () => {
     // this is the callback AFTER THE SECOND invokation of authenticate. 
     // authenticate is run once when you use google login, then again when it is redirected back to the authorized page
     // Once authenticate is invoked again, it goes here.
-    (issuer, profile, done) => {
-        console.log(profile);
-        const userID = profile.id;
+    // this should be async because it has the queries inside of it. If not async, it will run past and not wait for query
+    // causing MASSIVE headaches. MASSIVE. HEADACHES.
+    async (issuer, profile, done) => {
+        console.log('    Running passport verify function')
+        // console.log(profile);
+
+        //set all the variables that should be inside the user object from google oauth
+        const googleID = profile.id;
         const name = profile.displayName;
-        console.log(userID, name);
-        //find the userID in sql, if it exists, then store sql user ID in session
-        //WHY THE FUCK DOESN'T THIS WORK
-        query(`select * from users;`)
-        .then(query => {
-            console.log(query.rows);
+
+        //getting the sqlID ready
+        let sqlID = 0;
+
+        //find the userID in sql, if it exists, then store sqlID. It must wait for this query
+        await query(`SELECT * FROM users WHERE oauthkey='${String(googleID)}';`)
+        .then((queryData) => {
+            //crap out if too many users
+            if (queryData.rows.length > 1) {
+                console.log('Too many instances of same user');
+            }
+            //if found, "done" it
+            else if (queryData.rows.length === 1) {
+                console.log('    Found instance of user.');
+                sqlID = queryData.rows[0]._id;
+                //creates the user object to pass back to the session
+                const user = {
+                    "sqlID": sqlID,
+                    "name": name,
+                    "Google ID":googleID
+                }
+
+                //same as return, except it sends back user into req.session.passport
+                done(null, user);
+                
+            }
+            
+            //else create
+            else {
+                console.log(`    Didn't find ${name}. Inserting into users table`)
+                //if it doesn't exist, create new sql user with the name and google ID.
+                query(`INSERT INTO users (name, oauthkey) VALUES ('${name}', '${String(googleID)}');`)
+                .then(query => {
+                    console.log(`    Created user ${name}`);
+                })
+                //select user after it's created
+                .then(async () => {
+                    await query(`SELECT * FROM users WHERE oauthkey='${String(googleID)}';`)
+                    .then(query => {
+                        // console.log(query)
+                        sqlID = query.rows[0]._id;
+                        const user = {
+                            "sqlID": sqlID,
+                            "name": name,
+                            "Google ID":googleID
+                        }
+                        done(null, user);
+                    })
+                })
+                .catch(err => console.log(err));
+            }
         })
-        .then(() => next())
         .catch(err => console.log(err));
-        console.log('qery done');
-        //if it doesn't exist, create new sql user with the name and google ID.
-        
     }));
 }
 
