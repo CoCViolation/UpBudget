3530bbe401c466a0fca9bfc1280889a2bf004efe
Quyen Calixto <89818245+QtieCoder@users.noreply.github.com>
2022-01-04 10:12:59 -0500

    Update Budget Main component (#10)
    * added more styling to budget main
    
    * budgetmain update
    
    Co-authored-by: Quyen Calixto <quyencalixto@Quyens-MacBook-Pro.local>

--- client/components/Budget-main.jsx
+++ client/components/Budget-main.jsx
@@ -1,23 +1,173 @@
-import React, { useState } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
+// import { connect } from 'react-redux';
+import {Link} from 'react-router-dom';
 
 const BudgetMain = () => {
   return (
     <div className='container budget-container'>
-      <div className='header'>
-        <div className='header1'>Account</div>
-        <div className='header2'>Hi user!</div>
-      </div>
-      <div className='content-large'>Total Monthly Budget:</div>
-      <div className='content-large'>Spent</div>
-      <div className='content-large'>Budget</div>
+      <div className='header1'> My Account</div>
+      <div className='header2'> Hi User! </div>
 
-      <div className='left_space'>Total Monthly Budget:</div>
-      <div className='main'>
-        <div className='main1'>Main1</div>
+      <div className='content-large'>Total Monthly Budget:
+        
+        <ul className='categories'>
+          <table>
+            <thead>
+              <br></br>
+              <tr>
+                <th> Spending So far: </th>
+              </tr>
+ 
+            </thead>
+            <tbody>
+              <tr>
+              <td>Groceries</td>
+              <td>$</td>
+              </tr>
+              <tr>
+              <td>Gym</td>
+              <td>$</td>
+              </tr>
+              <tr>
+              <td>Rent</td>
+              <td>$</td>
+              </tr>
+              <tr>
+              <td>Car Loan</td>
+              <td>$</td>
+              </tr>
+              <tr>
+              <td>Fun Money</td>
+              <td>$</td>
+              </tr>
+              <tr>
+              <td>Groceries</td>
+              <td>$</td>
+              </tr>              <tr>
+              <td>Studen Loan</td>
+              <td>$</td>
+              </tr>
+              <tr>
+              <td>Electronics</td>
+              <td>$</td>
+              </tr>
+              <tr>
+              <td>Day Care</td>
+              <td>$</td>
+              </tr>
+              <tr>
+              <td>Clothing</td>
+              <td>$</td>
+              </tr>
+              <br></br>
+            </tbody>
+          </table>
+         
+        </ul>
       </div>
-      <div className='right_space'>Right_Space</div>
-    </div>
+      <div className='content-medium1'>Total Spent
+      <ul className='totalSpent'>
+          <table>
+            <thead>
+              <br></br>
+              <br></br>
+              <tr>
+                <th>  </th>
+              </tr>
+ 
+            </thead>
+            <tbody>
+              <tr>
+                <td>$</td>
+              </tr>
+              <tr>
+                <td>$</td>
+              </tr>
+              <tr>
+              <td>$</td>
+              </tr>
+              <tr>
+               <td>$</td>
+              </tr>
+              <tr>
+                <td>$</td>
+              </tr>
+              <tr>
+              <td>$</td>
+              </tr> <tr>
+               <td>$</td>
+              </tr>
+              <tr>
+                 <td>$</td>
+              </tr>
+              <tr>
+                <td>$</td>
+              </tr>
+              <tr>
+               <td>$</td>
+              </tr>
+              <br></br>
+            </tbody>
+          </table>
+         
+        </ul></div>
+      <div className='content-medium2'>Budget
+      <ul className='remBudget'>
+          <table>
+            <thead>
+              <br></br>
+              <br></br>
+              <tr>
+                <th>  </th>
+              </tr>
+ 
+            </thead>
+            <tbody>
+              <tr>
+                <td>$</td>
+              </tr>
+              <tr>
+                <td>$</td>
+              </tr>
+              <tr>
+              <td>$</td>
+              </tr>
+              <tr>
+               <td>$</td>
+              </tr>
+              <tr>
+                <td>$</td>
+              </tr>
+              <tr>
+              <td>$</td>
+              </tr> <tr>
+               <td>$</td>
+              </tr>
+              <tr>
+                 <td>$</td>
+              </tr>
+              <tr>
+                <td>$</td>
+              </tr>
+              <tr>
+               <td>$</td>
+              </tr>
+              <br></br>
+            </tbody>
+          </table>
+         
+        </ul></div>
+
+      <div className='buttonbox'>
+      <Link to='/totaldisplay'>
+      <button className="button">Input Spending</button>
+      </Link>
+      <br></br>
+      <Link to='/newbudget'>
+      <button className="button">Input New Budget</button>
+      </Link>
+      </div>
+     </div>
   );
 };
 
--- client/components/NewBudgetForm.jsx
+++ client/components/NewBudgetForm.jsx
@@ -1,7 +1,17 @@
 import React, { Component } from 'react';
+import useForm from 'react-hook-form';
 
 const NewBudgetForm = () => {
-  return <div>New Budget Goals!</div>;
+  return (
+    <div className="NewBudgetForm">
+
+      <form>
+        <input type="text" placeholder = "Groceries" name="groceries" className="type" />
+
+      </form> 
+    </div>
+ 
+  );
 };
 
 export default NewBudgetForm;
