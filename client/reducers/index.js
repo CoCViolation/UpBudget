d4a26fce3f84e48854ad47789b331753569e227e
Eugene Lee <40117342+scc135@users.noreply.github.com>
2022-01-04 21:40:00 -0500

    working (#16)

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { combineReducers } from 'redux';
 import budgetReducer from './budgetReducer.js';
 
@@ -14,20 +13,3 @@ const reducers = combineReducers({
 
 // make the combined reducers available for import
 export default reducers;
-=======
-// import { combineReducers } from 'redux';
-// // import ChatReducer from './chatReducer.js';
-
-// // import all reducers here
-// // import marketsReducer from './marketsReducer';
-
-// // combine reducers
-// const reducers = combineReducers({
-//   // if we had other reducers, they would go here
-//   // markets: marketsReducer,
-//   // chat: ChatReducer,
-// });
-
-// // make the combined reducers available for import
-// export default reducers;
->>>>>>> 1c942680eee19814dde8bd2dd9ff39846119b245
